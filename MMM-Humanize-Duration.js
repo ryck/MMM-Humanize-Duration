7fd94ba79d9f04c1c4c4fc26c571c6f10a24634c
Ricardo Gonzalez <rickgc@gmail.com>
2018-11-21 17:22:59 +0000

    🔥 First commit

---
+++ MMM-Humanize-Duration.js
@@ -0,0 +1,53 @@
+/* Magic Mirror Module: MMM-Humanize-Duration
+ * Version: 1.0.0
+ *
+ * By Ricardo Gonzalez https://github.com/ryck/MMM-Humanize-Duration
+ * MIT Licensed.
+ */
+Module.register("MMM-Humanize-Duration", {
+	defaults: {
+		updateInterval: 60 * 1 * 1000, // Every minute
+		animationSpeed: 500,
+		initialLoadDelay: 0, // start delay in milliseconds.
+		options:  {
+			round: true,
+			units: ["y", "mo", "w", "d"],
+			largest: 3,
+			conjunction: " and ",
+			serialComma: false
+		 },
+		debug: false
+	},
+	start: function() {
+		var self = this;
+		setInterval(function() {
+			self.updateDom(); // no speed defined, so it updates instantly.
+		}, this.config.updateInterval);
+	},
+	getStyles: function() {
+		return ["MMM-Humanize-Duration.css"];
+	},
+	// Define required scripts.
+	getScripts: function() {
+		return ["moment.js", this.file("node_modules/humanize-duration/humanize-duration.js")];
+	},
+	//Define header for module.
+	getHeader: function() {
+		return this.data.header;
+	},
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		if (!this.config.start) {
+			wrapper.innerHTML = "Please set the start date.";
+			wrapper.className = "dimmed light small";
+			return wrapper;
+		}
+		wrapper.className = "small bold align-right"
+		var start = moment(this.config.start);
+		var diff =  moment().diff(start)
+		var duration = humanizeDuration(diff, this.config.options)
+		wrapper.innerHTML = duration;
+		return wrapper;
+	}
+});
